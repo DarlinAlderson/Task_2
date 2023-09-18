e1ec8b563a9241a7c8c7a07d7d773aee4f2199f6
darlin_alderson <dash.cobain@gmail.com>
2023-09-18 21:03:19 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,28 @@
+import services.ToyStore;
+import models.Toy;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем экземпляр класса ToyStore
+        ToyStore toyStore = new ToyStore();
+
+        // Добавляем игрушки в магазин
+        toyStore.put("1 Мяч 10");
+        toyStore.put("2 Кукла 15");
+        toyStore.put("3 Машинка 12");
+
+        // Вызываем метод getToyById() 10 раз
+        for (int i = 0; i < 10; i++) {
+            int wonToyId = toyStore.getToyId();
+            Toy wonToy = toyStore.getToyById(wonToyId);
+            if (wonToy != null) {
+                System.out.println("Выиграна игрушка: " + wonToy.getName() + " (id: " + wonToy.getId() + ", вес: " + wonToy.getWeight() + ")");
+            } else {
+                System.out.println("Игрушки закончились");
+            }
+        }
+
+        // Сохраняем результаты в файл
+        toyStore.saveResultToFile("result.txt");
+    }
+}
